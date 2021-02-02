9859d905dea3d4f64a0ebb456d66e3b75d8be774
Daria <jadarya@mail.ru>
2021-02-02 23:20:24 +0900

    add constant - font

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+        ],
+        alias: {
+          src: './src',
+          assets: './src/assets',
+          helpers: './src/helpers',
+          components: './src/components',
+          navigation: './src/navigation',
+          screens: './src/screens',
+          store: './src/store',
+          libs: './src/libs',
+          i18n: './src/i18n',
+          utils: './src/utils',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/common/constants/fonts.ts
@@ -0,0 +1,13 @@
+export const fonts = {
+  bold: 'Gilroy-Bold',
+  light: 'Gilroy-Light',
+  lightItalic: 'Gilroy-LightItalic',
+  medium: 'Gilroy-Medium',
+  regular: 'Gilroy-Regular',
+  lightUltra: 'Gilroy-UltraLight',
+  trojanPro3: 'Trajan-Pro-3',
+  segoeUiLight: 'SegoeUI-Light',
+  sfProDisplay: 'SFProDisplay-Regular',
+  sfProText: 'SFProText-Regular',
+  sfProTextSemibold: 'SFProText-Semibold',
+};
