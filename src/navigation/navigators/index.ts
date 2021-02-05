7aa3b6949436ba8c5b0e26372ba4463ed7d31ff9
Daria <jadarya@mail.ru>
2021-02-06 04:43:50 +0900

    add navigators for manager

--- src/navigation/navigators/index.ts
+++ src/navigation/navigators/index.ts
@@ -4,3 +4,5 @@ export { default as MeetingsNavigator } from './MeetingsNavigators';
 export { default as ReceiptsNavigator } from './ReceiptsNavigator';
 export { default as SettingsNavigator } from './SettingsNavigator';
 export { default as QrNavigator } from './QrNavigator';
+export { default as MyPatriciansNavigator } from './MyPatriciansNavigator';
+export { default as InvitesToConsulatNavigator } from './InvitesToConsulatNavigator';
