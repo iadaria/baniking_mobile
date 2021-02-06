967070a8840de54abb5771f96a6a59a20a79d4ed
Daria <jadarya@mail.ru>
2021-02-07 08:52:19 +0900

    add auth constants

---
+++ src/app/store/auth/authReducer.ts
@@ -0,0 +1,9 @@
+/***************** Auth constants ********************/
+export const SIGN_IN_USER = 'auth/SIGN_IN_USER';
+export const SIGN_OUT_USER = 'auth/SIGN_OUT_USER';
+export const ENTER_CODE = 'auth/ENTER_CODE';
+export const RECOVERY_PASSWORD = 'auth/ENTER_CODE';
+
+/***************** Auth actions **********************/
+
+/***************** Auth reducer **********************/
