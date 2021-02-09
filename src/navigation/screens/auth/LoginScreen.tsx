200b85478897a4b6b9cb9485005f8b636d56cba3
Daria <jadarya@mail.ru>
2021-02-10 07:58:48 +0900

    add facebook sign in for android

--- src/navigation/screens/auth/LoginScreen.tsx
+++ src/navigation/screens/auth/LoginScreen.tsx
@@ -5,6 +5,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import { ParamListBase } from '@react-navigation/native';
 import { ICredential } from '../../../app/models/user';
 import VKLogin from 'react-native-vkontakte-login';
+import { LoginButton, AccessToken } from 'react-native-fbsdk';
 import { Button } from 'react-native';
 
 interface IProps {
@@ -34,12 +35,24 @@ export function LoginScreen({ socialLogin }: IProps) {
           console.log(auth);
         }}
       />
+
+      <Block margin={[0, 0, 3, 0]} />
+
+      <LoginButton
+        onLoginFinished={(error, result) => {
+          if (error) {
+            console.log('login has error: ' + result.error);
+          } else if (result.isCancelled) {
+            console.log('login is cancelled.');
+          } else {
+            AccessToken.getCurrentAccessToken().then((data) => {
+              console.log(data.accessToken.toString());
+            });
+          }
+        }}
+        onLogoutFinished={() => console.log('logout.')}
+      />
     </Block>
   );
 }
 
-/**
- * VK - https://github.com/doomsower/react-native-vkontakte-login
- * VK - https://github.com/doomsower/react-native-vkontakte-login/blob/master/docs/installation.md#android
- * VK - https://vk.com/dev/android_sdk
- */
