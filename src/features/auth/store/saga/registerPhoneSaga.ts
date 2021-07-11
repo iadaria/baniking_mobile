f5e44dc69e92522e4ff24c6ec6b1d7369f5e1e27
Daria <jadarya@mail.ru>
2021-07-11 18:55:22 +0900

    delete validated screens

--- src/features/auth/store/saga/registerPhoneSaga.ts
+++ src/features/auth/store/saga/registerPhoneSaga.ts
@@ -1,17 +1,23 @@
-import { ForkEffect, put, takeLatest } from 'redux-saga/effects';
+import { call, ForkEffect, put, takeLatest } from 'redux-saga/effects';
 import * as RootNavigation from '~/src/navigation/helpers/RootNavigation';
 import routes from '~/src/navigation/helpers/routes';
-import { ICredential } from '~/src/app/models/user';
+import { ICredential, VerifyPayload } from '~/src/app/models/user';
 import { methods, tokenToHeaders } from '~/src/app/api';
 import { getErrorStrings } from '~/src/app/utils/error';
 import {
   setPersistUserData,
   setPersistToken,
 } from '~/src/features/persist/store/appPersistActions';
-import { setAuthUserData } from '~/src/features/auth/store/authActions';
-import { PHONE_REGISTER } from '../authConstants';
+import {
+  requestSuccess,
+  setAuthUserData,
+} from '~/src/features/auth/store/authActions';
+import { EMAIL_REGISTER, VERIFY, COMPLETE_REGISTER } from '../authConstants';
 import { showAlert } from '~/src/app/common/components/showAlert';
 import { log, logline } from '~/src/app/utils/debug';
+import axios, { AxiosResponse } from 'axios';
+import { Fail } from '~/src/app/models/response';
+import { isSuccessStatus } from '../../../../app/models/response';
 
 // any - what we pass to call
 // second - what we return: void or string(return "done")
@@ -24,35 +30,39 @@ interface IAction {
   payload: ICredential;
 }
 
-interface IResult {
-  token: string;
-}
-
 function* registerPhoneSaga({
   payload,
 }: IAction) /* : Generator<Promise<ICredential>, void, IResult> */ {
   try {
-    log('payload *************', payload);
+    //__DEV__ && console.log('payload *************', payload);
 
     const { name, email, phone, agreement, device_name } = payload;
 
-    const { token }: IResult = yield methods.register(
+    const result: AxiosResponse = yield call(
+      methods.register,
       { name, email, phone, agreement, device_name },
       null,
     );
-    if (token) {
-      yield tokenToHeaders(token);
-      yield put(setPersistToken(token));
-      yield put(setPersistUserData({ email, name, phone }));
-      yield put(setAuthUserData({ token, email, name }));
-      yield RootNavigation.navigate(routes.navigators.DrawerNavigator, null);
+    log('[registerEmailSaga] result', result);
+    if (isSuccessStatus(result.status)) {
+      yield put(requestSuccess());
+      yield RootNavigation.navigate(routes.authNavigator.VerifyScreen, null);
+      /* if (token) {
+        yield tokenToHeaders(token);
+        yield put(setPersistToken(token));
+        yield put(setPersistUserData({ email, name, phone }));
+        yield put(setAuthUserData({ token, email, name }));
+        yield RootNavigation.navigate(routes.navigators.DrawerNavigator, null);
+      } */
     }
   } catch (e) {
-    log('[registerPhoneSaga]', e);
+    //if (axios.isAxiosError(e)) {
+    //const error: AxiosResponse<Fail> = e;
+    log('[registerEmailSaga/error]', e);
 
     let [errors, message, allErrors] = getErrorStrings(e);
 
-    logline('[registerPhoneSaga]', [errors, message]);
+    logline('[registerEmailSaga/error]', [errors, message]);
 
     const errorMessage = allErrors
       ? allErrors
@@ -62,10 +72,15 @@ function* registerPhoneSaga({
   }
 }
 
+type CompleteRegisterPayload = {
+  phone: string;
+  password: string;
+  password_confirmation: string;
+  device_name: string;
+};
+
 export default function* listener() {
-  yield takeLatest(PHONE_REGISTER, registerPhoneSaga);
-  //yield takeLatest(VERIFY, VerifySaga);
-  //yield takeLatest(COMPLETE_REGISER, registerCompleteSaga);
+  yield takeLatest(EMAIL_REGISTER, registerPhoneSaga);
 }
 
 /* {
@@ -78,4 +93,16 @@ export default function* listener() {
         "Пользователь с данным телефоном уже зарегистрирован"
       ]
   }
-} */
+}
+{
+    "message": "The given data was invalid.",
+    "errors": {
+        "phone": [
+            "Пользователь с данным телефоном уже зарегистрирован"
+        ],
+        "email": [
+            "Пользователь с данным электронным адресом уже зарегистрирован"
+        ]
+    }
+}
+*/
