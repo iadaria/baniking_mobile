97b520a584f7d70dac04d16aa5008f88105674b8
Daria <jadarya@mail.ru>
2021-07-15 08:27:23 +0900

    init change sms code

--- src/app/utils/common.ts
+++ src/app/utils/common.ts
@@ -3,8 +3,11 @@ import { appPatterns } from '~/src/app/common/constants/common';
 
 export const SEP_PAGE = 4;
 
-export const canLoadMore = (total: number, current: number, currentPage: number) =>
-  currentPage === 0 ? true : total > current;
+export const canLoadMore = (
+  total: number,
+  current: number,
+  currentPage: number,
+) => (currentPage === 0 ? true : total > current);
 
 export const getFileName = (file: string) => {
   return file.replace(appPatterns.filename, '');
@@ -14,7 +17,13 @@ export const replaceExtension = (file: string, ext: string) => {
   return file.replace(/\.[^/.]+$/, ext);
 };
 
-export function isElementExist(where: number[], what: number): [number | undefined, boolean] {
+export function isElementExist(
+  where: number[],
+  what: number,
+): [number | undefined, boolean] {
   const _indexOf = where.indexOf(what);
   return [_indexOf, _indexOf !== -1 && _indexOf !== undefined];
+}
+export function replaceAt(str: string, index: number, ch: string) {
+  return str.replace(/./g, (c, i) => (i == index ? ch : c));
 }
--- src/features/auth/screens/VerifyScreen/VerifyForm.tsx
+++ src/features/auth/screens/VerifyScreen/VerifyForm.tsx
@@ -1,24 +1,20 @@
-import React, { useEffect } from 'react';
-import { ScrollView, TextInput } from 'react-native';
+import React, { FC, useEffect } from 'react';
+import { ScrollView, TextInput, TextInputProps } from 'react-native';
 import { connect } from 'react-redux';
 import { ParamListBase } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Input, AppText, Block } from '~/src/app/common/components/UI';
-import { AppButton } from '~/src/app/common/components/UI/AppButton';
-import ValidatedElements from '~/src/app/common/components/ValidatedElements';
-import {
-  verify as verifyAction,
-  initVerifyInputs as initVerifyInputsAction,
-} from '~/src/features/auth/store/authActions';
+import { Block } from '~/src/app/common/components/UI';
+import { verify as verifyAction } from '~/src/features/auth/store/authActions';
 import { IRootState } from '~/src/app/store/rootReducer';
 import { IUserAuth } from '~/src/app/models/user';
 import { IVerifyInputs } from '../contracts/verifyInputs';
-import { colors, sizes } from '~/src/app/common/constants';
+import { colors } from '~/src/app/common/constants';
 import { log, logline } from '~/src/app/utils/debug';
 import { VerifyPayload } from '../../store/saga/verifySaga';
 import { styles as s } from './styles';
 import { useState, ForwardedRef } from 'react';
 import { color } from 'react-native-reanimated';
+import { replaceAt } from '../../../../app/utils/common';
 
 type RefInput = React.RefObject<TextInput>;
 
@@ -31,40 +27,59 @@ interface IProps {
   initVerifyInputs: (verifies: VerifyPayload) => void;
 }
 
-interface IDigitProps {
+interface IDigitProps extends TextInputProps {
   //ref: RefInput;
   prevRef?: RefInput;
   nextRef?: RefInput;
+  setDigit: (newDigit: string) => void;
+  digit: string;
 }
 
-const Digit = React.forwardRef(({ prevRef, nextRef }: IDigitProps, ref: ForwardedRef<TextInput>) => {
-  const [isFocus, setFocus] = useState(false);
+const Digit = React.forwardRef(
+  (
+    { prevRef, nextRef, digit, setDigit, ...otherProps }: IDigitProps,
+    ref: ForwardedRef<TextInput>,
+  ) => {
+    const [isFocus, setFocus] = useState(false);
 
-  function handleBlur() { setFocus(false); }
-  function handleFocus() { setFocus(true); }
+    function handleBlur() {
+      setFocus(false);
+    }
+    function handleFocus() {
+      setFocus(true);
+    }
 
-  return (
-    <TextInput
-      ref={ref}
-      style={[s.digit, isFocus && { backgroundColor: 'white' }]}
-      textAlign="center"
-      multiline={true}
-      keyboardType="numeric"
-      maxLength={1}
-      returnKeyType="next"
-      placeholder={!isFocus ? '-' : undefined}
-      placeholderTextColor={colors.white}
-      onChangeText={(text: string) => {
-        text.length > 0 && nextRef?.current?.focus();
-        text.length === 0 && prevRef?.current?.focus();
-      }}
-      onBlur={handleBlur}
-      onFocus={handleFocus}
-    />
-  )
-});
+    return (
+      <TextInput
+        ref={ref}
+        style={[s.digit, isFocus && { backgroundColor: 'white' }]}
+        textAlign="center"
+        multiline={true}
+        keyboardType="numeric"
+        maxLength={1}
+        returnKeyType="next"
+        value={digit}
+        placeholder={!isFocus ? '-' : undefined}
+        placeholderTextColor={colors.white}
+        onChangeText={(text: string) => {
+          setDigit(text);
+          text.length > 0 && nextRef?.current?.focus();
+          text.length === 0 && prevRef?.current?.focus();
+        }}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        {...otherProps}
+      />
+    );
+  },
+);
 
-const Code = () => {
+interface ICodeProps {
+  code: string;
+  setCode: (code: string) => void;
+}
+
+const Code: FC<ICodeProps> = ({ code, setCode }) => {
   const ref_one = React.createRef<TextInput>();
   const ref_two = React.createRef<TextInput>();
   const ref_three = React.createRef<TextInput>();
@@ -73,86 +88,64 @@ const Code = () => {
   const isErrors = false;
   const color = isErrors ? colors.error : colors.secondary;
 
+  function handleChangeCode(newDigit: string, at: number) {
+    const newCode = replaceAt(code, at, newDigit);
+    setCode(newCode);
+  }
+
   return (
     <Block style={[s.digits, { borderColor: color }]} row middle center>
-      <Digit ref={ref_one} nextRef={ref_two} />
-      <Digit ref={ref_two} nextRef={ref_three} prevRef={ref_one} />
-      <Digit ref={ref_three} nextRef={ref_four} prevRef={ref_two} />
-      <Digit ref={ref_four} prevRef={ref_three} />
+      <Digit
+        ref={ref_one}
+        nextRef={ref_two}
+        digit={code[0]}
+        setDigit={(digit: string) => handleChangeCode(digit, 0)}
+      />
+      <Digit
+        ref={ref_two}
+        nextRef={ref_three}
+        prevRef={ref_one}
+        digit={code[1]}
+        setDigit={(digit: string) => handleChangeCode(digit, 1)}
+      />
+      <Digit
+        ref={ref_three}
+        nextRef={ref_four}
+        prevRef={ref_two}
+        digit={code[2]}
+        setDigit={(digit: string) => handleChangeCode(digit, 2)}
+      />
+      <Digit
+        ref={ref_four}
+        prevRef={ref_three}
+        digit={code[3]}
+        setDigit={(digit: string) => handleChangeCode(digit, 4)}
+      />
     </Block>
   );
 };
-const VerifyFormContainer = ({
-  scrollViewRef,
-  currentUser,
-  verify,
-  initVerifyInputs,
-  defaultVerifyInputs,
-}: IProps): JSX.Element => {
-  // Use ref because don't need rendering component
-  const valuesRef = React.useRef<VerifyPayload>({
-    phone: currentUser?.phone || '',
-    action: 0,
-    code: '',
-  });
-  const [recreate, setRecreate] = React.useState<boolean>(true);
+const VerifyFormContainer = ({ currentUser, verify }: IProps): JSX.Element => {
+  const [code, setCode] = useState('1234');
+  const [recreate, setRecreate] = useState<boolean>(true);
 
   useEffect(() => {
-    if (currentUser) {
-      initVerifyInputs({
-        phone: currentUser?.phone || '',
-        action: 0,
-        code: '',
-      });
-    }
-  }, [currentUser, initVerifyInputs]);
+    logline('[VarifyForm] code = ', code);
+  }, []);
 
   const handleResetPassword = () => {
-    if (valuesRef.current) {
-      log('values1', valuesRef.current);
-      verify(valuesRef?.current);
-      //resetPassword(valuesRef?.current.email!);
-      setRecreate(!recreate);
-    }
+    //verify(valuesRef?.current);
+    //setRecreate(!recreate);
   };
 
-  return (
-    <ValidatedElements
-      key={Number(recreate)}
-      defaultInputs={defaultVerifyInputs}
-      scrollView={scrollViewRef}
-      valuesRef={valuesRef}>
-      <Code />
-
-      <Block row middle center>
-        <AppText primary semibold size={sizes.text.label}>
-          Code
-        </AppText>
-      </Block>
-      <Input
-        style={{ borderRadius: 10 }}
-        center
-        id="code"
-        placeholder="Enter code"
-      />
-      {/* Button */}
-      <AppButton margin={[2, 0, 1]} onPress={handleResetPassword}>
-        <AppText center medium>
-          Восстановить
-        </AppText>
-      </AppButton>
-    </ValidatedElements>
-  );
+  return <Code code={code} setCode={setCode} />;
 };
 
 const VerifyFormConnected = connect(
   ({ auth }: IRootState) => ({
     currentUser: auth.currentUser,
-    defaultVerifyInputs: auth.inputs.verify,
   }),
   {
     verify: verifyAction,
-    initVerifyInputs: initVerifyInputsAction,
   },
 )(VerifyFormContainer);
 
